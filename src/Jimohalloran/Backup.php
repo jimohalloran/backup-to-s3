54ad6cd4d353d4be9066f2f2c62b93f2eaa905d0
Jim O'Halloran <jim@jimohalloran.com>
2012-08-12 20:54:50 +0930

    Implemented mysqldump'ing of databases.

--- src/Jimohalloran/Backup.php
+++ src/Jimohalloran/Backup.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Jimohalloran;
+use Symfony\Component\Process\Process;
 
 /**
  * Core backup class
@@ -12,6 +13,7 @@ class Backup {
 	protected $_config;
 	
 	protected $_tmpPath;
+	protected $_tmpDbPath;
 	
 	public function __construct($yamlConfig) {
 		$this->_config = $yamlConfig;
@@ -23,6 +25,42 @@ class Backup {
 	
 	public function execute() {
 		$this->_createTmpFolder();
+		
+		if (count($this->_config['database'])) {
+			$this->_createDbFolder();
+			foreach ($this->_config['database'] as $name => $connectionInfo) {
+				$this->_mysqlDump($name, $connectionInfo);
+			}
+		}
+	}
+	
+	protected function _mysqlDump($name, $conn) {
+		$cmd = 'mysqldump';
+		$cmd .= ' -h '.$this->_elem($conn, 'hostname', 'localhost');
+		$cmd .= ' -u '.$this->_elem($conn, 'username', 'root');
+		$cmd .= array_key_exists('password', $conn) ? ' -p' .$conn['password'] : '';
+		$cmd .= ' ' . $this->_elem($conn, 'database', '') . ' > ' . $this->_tmpDbPath . '/' . $name . '.sql';
+
+		$process = new Process($cmd);
+		$process->setTimeout(3600);
+		$process->run();
+		if (!$process->isSuccessful()) {
+				throw new BackupException($process->getErrorOutput());
+		}
+		print $process->getOutput();		
+	}
+	
+	protected function _elem($array, $key, $default) {
+		if (array_key_exists($key, $array)) {
+			return $array[$key];
+		} else {
+			return $default;
+		}
+	}
+	
+	protected function _createDbFolder() {
+		$this->_tmpDbPath = $this->_tmpPath.'/database';
+		mkdir($this->_tmpDbPath, 0700);
 	}
 	
 	protected function _createTmpFolder() {
